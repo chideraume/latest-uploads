846a46b7fb14b7ccc8a86a5229881b393d2aef70
chideraume <92882366+chideraume@users.noreply.github.com>
2022-02-16 10:28:34 +0100

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,5 +4,17 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  } 
+
+  getHello2(): {} {
+    return {message: 'Hello World!', title: 'My Greeting App'};
   }
+
+
+  getHome(): {} {
+    return {title: 'Home Page'};
+    }
+    getAboutUs(): {} {
+    return {title: 'About Us Page'};
+    }
 } 
